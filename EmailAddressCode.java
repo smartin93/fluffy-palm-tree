267a732f1be3befeaaf1e61262d5e000b2af8f22
smartin93 <60673320+smartin93@users.noreply.github.com>
2023-03-01 14:37:44 -0500

    Add files via upload

---
+++ EmailAddressCode.java
@@ -0,0 +1,28 @@
+/*Determine whether a string is a valid email address or not */
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class emailaddresscode {
+  public static void main(String[] args) {
+
+  
+  String Email = "";
+
+  Scanner keyboard = new Scanner(System.in);
+  System.out.println("Enter email address to check if valid");
+  Email = keyboard.nextLine();
+    
+//boolean PatternMatch = Pattern.matches("([a-zA-Z0-9]+|[#!%$‘&+*–/= ?^_`{|}~])+.([a-zA-Z0-9]|[#!%$‘&+*–/= ?^_`{|}~])+", Email);
+
+ matches problem 1, 2 and 3
+boolean PatternMatch = 
+Pattern.matches("([a-zA-Z0-9]+|[#!%$‘&+*–/= ?^_`{|}~])+.([a-zA-Z0-9]|[#!%$‘&+*–/= ?^_`{|}~])+@( [a-zA-Z0-9]+|[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]).([a-zA-Z0-9]+|[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9])", Email);
+
+    
+    if(PatternMatch) {
+      System.out.println("Valid Email Address ");
+    } else {
+      System.out.println("Invalid Email Address");
+  }
+}
+}
